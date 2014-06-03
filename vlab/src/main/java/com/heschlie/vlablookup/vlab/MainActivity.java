2f47c5973279b2ead1fc5af9b1397821e21ca41a
Stephen Schlie <heschlie@gmail.com>
2014-06-03 10:13:31 -0700

    pulling in only one example from json.lab to test with

--- vlab/src/main/java/com/heschlie/vlablookup/vlab/MainActivity.java
+++ vlab/src/main/java/com/heschlie/vlablookup/vlab/MainActivity.java
@@ -3,17 +3,10 @@ package com.heschlie.vlablookup.vlab;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -22,7 +15,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.StringBufferInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -32,32 +24,30 @@ public class MainActivity extends ListActivity {
     private ProgressDialog pDialog;
 
     // URL to get JSON
-    private static String url = "http://api.androidhive.info/contacts/";
+    private static String url = "http://json.lab.nbttech.com/v1/resources/names/oak-sh100";
 
     // JSON Node names
-    private static final String TAG_CONTACTS = "contacts";
-    private static final String TAG_ID = "id";
     private static final String TAG_NAME = "name";
-    private static final String TAG_EMAIL = "email";
-    private static final String TAG_ADDRESS = "address";
-    private static final String TAG_GENDER = "gender";
-    private static final String TAG_PHONE = "phone";
-    private static final String TAG_MOBILE = "mobile";
-    private static final String TAG_HOME = "home";
-    private static final String TAG_OFFICE = "office";
+    private static final String TAG_TERMSRV = "termsrv";
+    private static final String TAG_TERMSRV_PORT = "termsrv_port";
+    private static final String TAG_LOCATION = "location";
+    private static final String TAG_ALTITUDE = "location_altitude";
+    private static final String TAG_OWNER = "owner";
+    private static final String TAG_RPB = "rpb";
+    private static final String TAG_RPB_PLUG = "rpb_plug";
 
     // contacts JSONArray
     JSONArray contacts = null;
 
     // Hashmap for Listview
-    ArrayList<HashMap<String, String>> contactList;
+    ArrayList<HashMap<String, String>> deviceList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        contactList = new ArrayList<HashMap<String, String>>();
+        deviceList = new ArrayList<HashMap<String, String>>();
         ListView lv = getListView();
 
         new GetContacts().execute();
@@ -107,39 +97,32 @@ public class MainActivity extends ListActivity {
 
             if (jsonStr != null) {
                 try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    JSONObject d = new JSONObject(jsonStr);
 
-                    // Getting JSON array node
-                    contacts = jsonObj.getJSONArray(TAG_CONTACTS);
+                    String name = d.getString(TAG_NAME);
+                    String location = d.getString(TAG_LOCATION);
+                    String altitude = d.getString(TAG_ALTITUDE);
+                    String termsrv = d.getString(TAG_TERMSRV);
+                    String termsrvPort = d.getString(TAG_TERMSRV_PORT);
+                    String rpb = d.getString(TAG_RPB);
+                    String rpbPlug = d.getString(TAG_RPB_PLUG);
+                    String owner = d.getString(TAG_OWNER);
 
-                    // Loop through all contacts
-                    for (int i = 0; i < contacts.length(); i++) {
-                        JSONObject c = contacts.getJSONObject(i);
+                    // tmp map for single device
+                    HashMap<String, String> device = new HashMap<String, String>();
 
-                        String id = c.getString(TAG_ID);
-                        String name = c.getString(TAG_NAME);
-                        String email = c.getString(TAG_EMAIL);
-                        String address = c.getString(TAG_ADDRESS);
-                        String gender = c.getString(TAG_GENDER);
+                    // adding each child node to map
+                    device.put(TAG_NAME, name);
+                    device.put(TAG_LOCATION, location);
+                    device.put(TAG_ALTITUDE, altitude);
+                    device.put(TAG_TERMSRV, termsrv);
+                    device.put(TAG_TERMSRV_PORT, termsrvPort);
+                    device.put(TAG_RPB, rpb);
+                    device.put(TAG_RPB_PLUG, rpbPlug);
+                    device.put(TAG_OWNER, owner);
 
-                        // Phone node is JSON Object
-                        JSONObject phone = c.getJSONObject(TAG_PHONE);
-                        String mobile = phone.getString(TAG_MOBILE);
-                        String home = phone.getString(TAG_HOME);
-                        String office = phone.getString(TAG_OFFICE);
-
-                        // tmp map for single contact
-                        HashMap<String, String> contact = new HashMap<String, String>();
-
-                        // adding each child node to map
-                        contact.put(TAG_ID, id);
-                        contact.put(TAG_NAME, name);
-                        contact.put(TAG_EMAIL, email);
-                        contact.put(TAG_MOBILE, mobile);
-
-                        // adding contact to list
-                        contactList.add(contact);
-                    }
+                    // adding device to list
+                    deviceList.add(device);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -159,9 +142,9 @@ public class MainActivity extends ListActivity {
 
             // update parsed json into listview
             ListAdapter adapter = new SimpleAdapter(
-                    MainActivity.this, contactList, R.layout.list_item,
-                    new String[] { TAG_NAME, TAG_EMAIL, TAG_MOBILE },
-                    new int[] { R.id.name, R.id.email, R.id.mobile }
+                    MainActivity.this, deviceList, R.layout.list_item,
+                    new String[] { TAG_NAME, TAG_LOCATION, TAG_ALTITUDE },
+                    new int[] { R.id.name, R.id.location, R.id.altitude}
             );
 
             setListAdapter(adapter);
