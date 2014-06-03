3dbd3878f9eee0904009719c4fc26e5bab2d2ab1
Stephen Schlie <heschlie@gmail.com>
2014-06-03 09:03:59 -0700

    initial commit

---
+++ vlab/src/main/java/com/heschlie/vlablookup/vlab/MainActivity.java
@@ -0,0 +1,186 @@
+package com.heschlie.vlablookup.vlab;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringBufferInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MainActivity extends ListActivity {
+
+    private ProgressDialog pDialog;
+
+    // URL to get JSON
+    private static String url = "http://api.androidhive.info/contacts/";
+
+    // JSON Node names
+    private static final String TAG_CONTACTS = "contacts";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_EMAIL = "email";
+    private static final String TAG_ADDRESS = "address";
+    private static final String TAG_GENDER = "gender";
+    private static final String TAG_PHONE = "phone";
+    private static final String TAG_MOBILE = "mobile";
+    private static final String TAG_HOME = "home";
+    private static final String TAG_OFFICE = "office";
+
+    // contacts JSONArray
+    JSONArray contacts = null;
+
+    // Hashmap for Listview
+    ArrayList<HashMap<String, String>> contactList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactList = new ArrayList<HashMap<String, String>>();
+        ListView lv = getListView();
+
+        new GetContacts().execute();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // showing dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please Wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+
+            Log.d("Response: ", "> " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON array node
+                    contacts = jsonObj.getJSONArray(TAG_CONTACTS);
+
+                    // Loop through all contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String id = c.getString(TAG_ID);
+                        String name = c.getString(TAG_NAME);
+                        String email = c.getString(TAG_EMAIL);
+                        String address = c.getString(TAG_ADDRESS);
+                        String gender = c.getString(TAG_GENDER);
+
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject(TAG_PHONE);
+                        String mobile = phone.getString(TAG_MOBILE);
+                        String home = phone.getString(TAG_HOME);
+                        String office = phone.getString(TAG_OFFICE);
+
+                        // tmp map for single contact
+                        HashMap<String, String> contact = new HashMap<String, String>();
+
+                        // adding each child node to map
+                        contact.put(TAG_ID, id);
+                        contact.put(TAG_NAME, name);
+                        contact.put(TAG_EMAIL, email);
+                        contact.put(TAG_MOBILE, mobile);
+
+                        // adding contact to list
+                        contactList.add(contact);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            // dismiss the progress dialog
+            if (pDialog.isShowing()) {
+                pDialog.dismiss();
+            }
+
+            // update parsed json into listview
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList, R.layout.list_item,
+                    new String[] { TAG_NAME, TAG_EMAIL, TAG_MOBILE },
+                    new int[] { R.id.name, R.id.email, R.id.mobile }
+            );
+
+            setListAdapter(adapter);
+        }
+    }
+
+//    /**
+//     * A placeholder fragment containing a simple view.
+//     */
+//    public static class PlaceholderFragment extends Fragment {
+//
+//        public PlaceholderFragment() {
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                Bundle savedInstanceState) {
+//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+//            return rootView;
+//        }
+//    }
+}
